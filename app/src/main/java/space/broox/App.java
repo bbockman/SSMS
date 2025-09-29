a74c803f3e4b1f7f038eda84778ef2101128cac4
broox <bwbockman@broox.space>
2025-09-28 23:36:12 -0400

    Reworked to WAR format, and included site statics

---
+++ app/src/main/java/space/broox/App.java
@@ -0,0 +1,14 @@
+package space.broox;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) throws Exception {
+
+        logger.atInfo().log("Starting application...");
+
+    }
+}
---
+++ auth/src/main/java/space/broox/auth/PlayAuth.java
@@ -0,0 +1,7 @@
+package space.broox.auth;
+
+public class PlayAuth {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Starting PlayAuth...");
+    }
+}
